f3c41b7ac957a7e0120de31693ad7ff9a55c24ff
xiananxi <1848511983@qq.com>
2024-05-10 12:11:12 +0800

    .c file

---
+++ china2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char  c1='C';
+	char  c2='h';
+	char  c3='i';
+	char  c4='n';
+	char  c5='a';
+	
+	c1=c1+4;
+	c2=c2+4;
+	c3=c3+4;
+	c4=c4+4;
+	c5=c5+4;
+	
+	
+	putchar(c1);
+	putchar(c2);
+	putchar(c3);
+	putchar(c4);
+	putchar(c5);
+	
+	return 0;
+	
+}
